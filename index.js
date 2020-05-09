605f05ddaf5e964a3efc6dbda27cfcf33cfa166c
kntrieu <kntrieuit@gmail.com>
2020-05-09 14:06:59 +0700

    Init files for the first time

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require ("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from server");
+});
+
+app.listen(5000, () => {
+    console.log("App listening on port 5000");
+});
